3afce2dc81064973dff1765b02aa3a2f40ab19ae
manuelalanis <alanism@uabc.edu.mx>
2016-05-09 03:22:21 -0700

    directorio de imagenes

--- upload.php
+++ upload.php
@@ -7,10 +7,10 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "El archivo es una imagen - " . $check["mime"] . ".";
+        // echo "El archivo es una imagen - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "El archivo no es una imagen.";
+        // echo "El archivo no es una imagen.";
         $uploadOk = 0;
     }
 }
@@ -35,8 +35,29 @@ if ($uploadOk == 0) {
     echo "Disculpa, tu foto no fue subida";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "La imagen ". basename( $_FILES["fileToUpload"]["name"]). " se ha subido correctamente";
+    if (
+        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        // echo "La imagen ". basename( $_FILES["fileToUpload"]["name"]). " se ha subido correctamente";
+
+        
+        $data = $_FILES["fileToUpload"]["name"];
+
+        $inp = file_get_contents('dir.json');
+        $tempArray = json_decode($inp);
+        
+        array_push($tempArray, $data);
+        //echo $data;
+        
+        //echo json_encode($tempArray);
+        $jsonData = json_encode($tempArray);
+        file_put_contents('dir.json', $jsonData);
+        // $rows = array();
+        // while($r = mysqli_fetch_assoc($resultado)) {
+        //     $rows[] = $r;
+        // }
+        print json_encode($_FILES["fileToUpload"]["name"]);
+        //Convertir
+        //Push
     } else {
         echo "Disculpa, hubo un error subiendo tu imagen.";
     }
